7a12e67643e0ca2ce76dd0abd409126c95680b58
Kyle Shive <shivekyle@gmail.com>
2018-05-08 09:31:19 -0500

    cleaned up code

--- expressions.py
+++ expressions.py
@@ -1,25 +1,3 @@
-"""
-def expression_matter(a, b, c):
-    array_of_answers = []
-    sol = a * (b + c)
-    sol2 = a * b * c
-    sol3 = a + b * c
-    sol4 = (a + b) * c
-    sol5 = a + b + c
-    sol6 = a * b + c
-    array_of_answers.append(sol)
-    array_of_answers.append(sol2)
-    array_of_answers.append(sol3)
-    array_of_answers.append(sol4)
-    array_of_answers.append(sol5)
-    array_of_answers.append(sol6)
-    print (array_of_answers)
-    array_of_answers = sorted(array_of_answers)
-    return (array_of_answers[-1])
-
-"""
-
-# old method above, more efficient method below
 
 def expression_matter(a, b, c):
     print (max(a*b*c, a+b+c, (a+b)*c, a*(b+c) ) )
