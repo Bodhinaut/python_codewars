8381f43a6d1d955eafe8a8a3eb69ac20a4d50690
Kyle Shive <shivekyle@gmail.com>
2018-05-15 13:04:53 -0500

    refactored

--- evaporator.py
+++ evaporator.py
@@ -1,11 +1,11 @@
 def evaporator(content, evap_per_day, threshold):
     n = 0
     current = 100
-    percent = 1 - evap_per_day / 100.0
+    percent = 1 - (evap_per_day / 100.0)
     while current > threshold:
         current *= percent
         n += 1
-    return n
+    print (n)
 
             
 
