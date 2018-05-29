d35b5b0deefa63a4c693ed559af1ec1f51b4c7aa
Kyle Shive <shivekyle@gmail.com>
2018-05-29 10:24:58 -0500

    initial verison

---
+++ solving_num.py
@@ -0,0 +1,10 @@
+value = []
+items = [x for x in input("Please input 4 binary numbers seperated by commas: ").split(',')]
+
+for p in items:
+	intp = int(p, 2)
+	if not intp % 5:
+		value.append(p)
+
+print (','.join(value))
+
